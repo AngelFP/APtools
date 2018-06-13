1846b5f1eee58f67c2f1b09e954f9275d362321b
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2018-06-13 15:21:33 +0200

    Add setup.py

---
+++ README.md
@@ -0,0 +1,1 @@
+Collection of modules containing typical functions, equations, relations, etc. used in accelerator physics.
---
+++ setup.py
@@ -0,0 +1,35 @@
+import sys
+from setuptools import setup, find_packages
+
+import aptools
+
+# Read long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+def read_requirements():
+    with open('requirements.txt') as f:
+        return [line.strip() for line in f.readlines()]
+
+# Main setup command
+setup(name='APtools',
+      version=aptools.__version__,
+      author='Angel Ferran Pousa',
+      author_email="angel.ferran.pousa@desy.de",
+      description='Coollections of tools for accelerator physics',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/AngelFP/VisualPIC',
+      license='GPLv3',
+      packages=find_packages('.'),
+      install_requires=read_requirements(),
+      platforms='any',
+      classifiers=(
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python :: 3",
+          "Intended Audience :: Science/Research",
+          "Topic :: Scientific/Engineering :: Physics",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent"),
+      )
+
