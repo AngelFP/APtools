a88f1d865e152cb76536e293898af6e5949f3ea5
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2018-07-30 18:21:12 +0200

    Formatting

--- aptools/plotting/quick_diagnostics.py
+++ aptools/plotting/quick_diagnostics.py
@@ -17,12 +17,16 @@ def phase_space_overview(x, y, z, px, py, pz, q):
     plt.plot(x * 1e6, px, '.', ms=1)
     plt.xlabel("x [$\\mu m$]")
     plt.ylabel("$p_x \\ \\mathrm{[m_ec^2/e]}$")
-    plt.text(0.3, 0.9, '$\\epsilon_{n,x} = $' + '{}'.format(np.around(em_x, 3)) + '$\\ \\mathrm{\\mu m \\ rad}$', transform=ax_1.transAxes, fontsize=8)
+    plt.text(0.3, 0.9, '$\\epsilon_{n,x} = $' + '{}'.format(np.around(em_x, 3))
+             + '$\\ \\mathrm{\\mu m \\ rad}$', transform=ax_1.transAxes,
+             fontsize=8)
     ax_2 = plt.subplot(132)
     plt.plot(y * 1e6, py, '.', ms=1)    
     plt.xlabel("y [$\\mu m$]")
     plt.ylabel("$p_y \\ \\mathrm{[m_ec^2/e]}$")
-    plt.text(0.3, 0.9, '$\\epsilon_{n,y} = $' + '{}'.format(np.around(em_y, 3)) + '$\\ \\mathrm{\\mu m \\ rad}$', transform=ax_2.transAxes, fontsize=8)
+    plt.text(0.3, 0.9, '$\\epsilon_{n,y} = $' + '{}'.format(np.around(em_y, 3))
+             + '$\\ \\mathrm{\\mu m \\ rad}$', transform=ax_2.transAxes,
+             fontsize=8)
     ax_3 = plt.subplot(133)
     plt.plot(dz / ct.c * 1e15, pz, '.', ms=1)
     plt.xlabel("$\\Delta z$ [fs]")
