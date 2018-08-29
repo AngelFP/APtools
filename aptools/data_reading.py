e0d243889528484a96a17b28f918077378043f07
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2018-08-29 15:50:15 +0200

    Implement openPMD data reading

--- aptools/data_reading.py
+++ aptools/data_reading.py
@@ -3,6 +3,9 @@ tracking codes"""
 
 import numpy as np
 import scipy.constants as ct
+from h5py import File as H5F
+
+from aptools.helper_functions import join_infile_path
 
 
 def read_csrtrack_data_fmt1(file_path):
@@ -60,3 +63,47 @@ def read_astra_data(file_path):
     pz[1:] += pz[0]
     q = data[:,7] * 1e-9
     return x, y, z, px, py, pz, q
+
+def read_openpmd_beam(file_path, species_name):
+    """Reads particle data from a h5 file following the openPMD standard and
+    returns it in the unis used by APtools.
+
+    Parameters:
+    -----------
+    file_path : str
+        Path to the file with particle data
+
+    species_name : str
+        Path to the file with particle data
+
+    Returns:
+    --------
+    A tuple with 7 arrays containing the 6D phase space and charge of the
+    particles.
+    """
+    file_content = H5F(file_path)
+    # get base path in file
+    iteration = list(file_content['/data'].keys())[0]
+    base_path = '/data/{}'.format(iteration)
+    # get path under which particle data is stored
+    particles_path = file_content.attrs['particlesPath'].decode()
+    # get species
+    beam_species = file_content[
+        join_infile_path(base_path, particles_path, species_name) ]
+    # get data
+    m = beam_species['mass'].attrs['value']
+    q = beam_species['charge'].attrs['value']
+    x = (beam_species['position/x'][:]
+         + beam_species['positionOffset/x'].attrs['value'])
+    y = (beam_species['position/y'][:]
+         + beam_species['positionOffset/y'].attrs['value'])
+    z = (beam_species['position/z'][:]
+         + beam_species['positionOffset/z'].attrs['value'])
+    px = beam_species['momentum/x'][:] / (m*ct.c)
+    py = beam_species['momentum/y'][:] / (m*ct.c)
+    pz = beam_species['momentum/z'][:] / (m*ct.c)
+    w = beam_species['weighting'][:]
+    q *= w
+    print(q.sum())
+    return x, y, z, px, py, pz, q
+
--- aptools/helper_functions.py
+++ aptools/helper_functions.py
@@ -19,3 +19,24 @@ def weighted_std(values, weights=1):
     mean_val = np.average(values, weights=np.abs(weights))
     std = np.sqrt(np.average((values-mean_val)**2, weights=np.abs(weights)))
     return std
+
+def join_infile_path(*paths):
+    """
+    Join path components using '/' as separator.
+    This method is defined as an alternative to os.path.join, which uses '\\'
+    as separator in Windows environments and is therefore not valid to navigate
+    within data files.
+
+    Parameters:
+    -----------
+    *paths: all strings with path components to join
+
+    Returns:
+    --------
+    A string with the complete path using '/' as separator.
+    """
+    # Join path components
+    path = '/'.join(paths)
+    # Correct double slashes, if any is present
+    path = path.replace('//', '/')
+    return path
