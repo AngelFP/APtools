Fix charge calculation in HiPACE

aptools/data_handling/reading.py
@@ -179,8 +179,13 @@ def read_hipace_beam(file_path, plasma_dens):
     """
     s_d = plasma_skin_depth(plasma_dens)
     file_content = H5File(file_path)
+    # sim parameters
+    n_cells = file_handle.attrs['NX']
+    sim_size = (file_handle.attrs['XMAX'] - file_handle.attrs['XMIN'])
+    cell_vol = np.prod(sim_size/n_cells)
+    q_norm = cell_vol * plasma_dens * 1e6 * s_d**3 * ct.e
     # get data
-    q = np.array(file_content.get('q')) * ct.e
+    q = np.array(file_content.get('q')) * q_norm
     x = np.array(file_content.get('x2')) * s_d
     y = np.array(file_content.get('x3')) * s_d
     z = np.array(file_content.get('x1')) * s_d