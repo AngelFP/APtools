d129e859b536e6ab11c538beca3685feab0669ca
Angel Ferran Pousa <angel.ferran.pousa@desy.de>
2021-08-18 14:53:34 +0200

    Formatting

--- aptools/plotting/rc_params.py
+++ aptools/plotting/rc_params.py
@@ -4,18 +4,18 @@ from matplotlib import rcParamsDefault
 
 
 custom_rc_params = {'axes.linewidth': 0.5,
-                     'axes.labelsize': 8,
-                     'xtick.major.size': 2,
-                     'ytick.major.size': 2,
-                     'xtick.major.width': 0.5,
-                     'ytick.major.width': 0.5,
-                     'xtick.labelsize': 8,
-                     'ytick.labelsize': 8,
-                     'xtick.direction': 'in',
-                     'ytick.direction': 'in',
-                     'xtick.top': True,
-                     'ytick.right': True,
-                     'legend.borderaxespad': 1}
+                    'axes.labelsize': 8,
+                    'xtick.major.size': 2,
+                    'ytick.major.size': 2,
+                    'xtick.major.width': 0.5,
+                    'ytick.major.width': 0.5,
+                    'xtick.labelsize': 8,
+                    'ytick.labelsize': 8,
+                    'xtick.direction': 'in',
+                    'ytick.direction': 'in',
+                    'xtick.top': True,
+                    'ytick.right': True,
+                    'legend.borderaxespad': 1}
 
 
 rc_params = {**rcParamsDefault, **custom_rc_params}
