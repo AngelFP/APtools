1421ea9d4a7615231ed244020cb5c5149621c1b3
Ángel Ferran Pousa <angel.ferran.pousa@desy.de>
2023-03-09 22:45:03 +0100

    Expose main methods in module root

---
+++ aptools/particle_distributions/__init__.py
@@ -0,0 +1,6 @@
+from .particle_distribution import ParticleDistribution
+from .read import read_distribution
+from .save import save_distribution
+
+
+__all__ = ['ParticleDistribution', 'read_distribution', 'save_distribution']
